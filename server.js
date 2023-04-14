35929b51d8db3fd7b79702e4e05785bf151942d3
tenorio201 <00x01x00@gmail.com>
2023-04-15 02:59:16 +0900

    First Commit!

--- server.js
+++ server.js
@@ -1,51 +1,50 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.json());
 
-const apiKey = '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';
+const apiKey = process.env.OPENAI_API_KEY;
 
 app.post('/chat', async (req, res) => {
-	const userInput = req.body.message;
-const userInput2=req.body.message2;
-	const userInput3=req.body.message3;
-	const number=parseInt(userInput3);
-	try {
-		const response = await axios.post(
-			'https://api.openai.com/v1/chat/completions',
+  const userInput = req.body.message;
+  const userInput2 = req.body.message2;
+  const responseAmount = parseInt(req.body.message3, 10) || 1;
 
-			{
-				model: 'gpt-3.5-turbo',
-				messages: [
-					{ role: 'system', content: userInput2 },
-					{ role: 'user', content: userInput },
-				],
-				max_tokens: 4000,
-				n: number,
-				stop: null,
-				temperature: 0.9,
-			},
-			{
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${apiKey}`,
-				},
-			}
-		);
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: userInput2 },
+          { role: 'user', content: userInput },
+        ],
+        max_tokens: 2000,
+        n: responseAmount,
+        stop: null,
+        temperature: 0.9,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }
+    );
 
-	const gptResponses = response.data.choices.map(choice => choice.message.content.trim());
-		res.json({ messages: gptResponses });
+    const gptResponses = response.data.choices.map((choice) => choice.message.content.trim());
+    res.json({ messages: gptResponses });
 
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: 'Error fetching GPT-3 response' });
-	}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching GPT-3 response' });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
 
